204c4c30cb7fb82cc7d79a8c2502bcc7bbe3e5b0
bryanneva <bryan.neva@gmail.com>
2018-07-16 21:48:59 -0400

    Add spring

---
+++ src/main/kotlin/com/timobot/matchhandler/MatchhandlerApplication.kt
@@ -0,0 +1,11 @@
+package com.timobot.matchhandler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatchhandlerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MatchhandlerApplication>(*args)
+}
